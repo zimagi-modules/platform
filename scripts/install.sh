29ba0745eb11565fa4082997997c29c5024b4711
Adrian Webb <adrian@webb.sh>
2022-11-05 20:43:14 -0400

    Adding Terraform installation to the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 #
-# Install module related dependencies
+# Install cluster management related packaging
 #
 set -e
+
+TERRAFORM_VERSION=1.3.3
+
+if [ ! -f /usr/local/bin/terraform ]
+then
+    echo "Installing Terraform version ${TERRAFORM_VERSION}"
+    wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip 2>/dev/null
+    unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin
+fi
+mkdir -p ~/.terraform.d/plugins
