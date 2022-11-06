29460c0b6a26afb5962afeaef1b72710584cce7d
Adrian Webb <adrian@webb.sh>
2022-11-05 20:44:37 -0400

    Adding Terraform and platform project related settings in the Django settings file.

--- django.py
+++ django.py
@@ -1,4 +1,17 @@
+from django.conf import settings
+
 from settings.config import Config
+
 #
 # Module environment configurations
 #
+
+#
+# Platform configuration
+#
+settings.PROJECT_PATH_MAP['platform_path'] = 'platforms'
+
+#
+# Terraform configuration
+#
+TERRAFORM_MAX_PROCESSES = Config.integer('ZIMAGI_TERRAFORM_MAX_PROCESSES', 10)
