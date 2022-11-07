033df5581aba4a1d8448f0c2dfdb1a339ecbaef1
Adrian Webb <adrian@webb.sh>
2022-11-07 01:06:33 -0500

    Adding platform variables in the Django settings file.

--- django.py
+++ django.py
@@ -3,13 +3,17 @@ from django.conf import settings
 from settings.config import Config
 
 #
-# Module environment configurations
-#
-
-#
 # Platform configuration
 #
 settings.PROJECT_PATH_MAP['platform_path'] = 'platforms'
+
+PLATFORM_HOST_VARIABLE = 'zimagi_host'
+PLATFORM_COMMAND_API_PORT_VARIABLE = 'zimagi_command_port'
+PLATFORM_DATA_API_PORT_VARIABLE = 'zimagi_data_port'
+
+PLATFORM_SSH_HOST_VARIABLE = 'ssh_host'
+PLATFORM_SSH_PORT_VARIABLE = 'ssh_port'
+PLATFORM_SSH_USER_VARIABLE = 'ssh_user'
 
 #
 # Terraform configuration
